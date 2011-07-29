03ae716cf632a91f3b4634d94e6574ab0e27bc2b
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2011-07-29 09:20:07 +0300

    Bug fixes

--- reduce.R
+++ reduce.R
@@ -221,7 +221,7 @@ while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0) {
 
 				# Write out the construct scores first
 				
-				for(i in 1:nrow(thisResults$constructs)
+				for(i in 1:nrow(thisResults$constructs)){
 					cat("S",replication,designNumber,analysis,thisResults$constructs[i,],thisResults$constructs[i,],sep="\t")
 					cat("\n")
 				}
